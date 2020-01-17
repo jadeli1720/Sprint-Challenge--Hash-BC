f7a1f6dd5350be2a408b05d6ff47962f9bc4e3ee
Jade E Lopez <38263897+jadeli1720@users.noreply.github.com>
2020-01-17 14:18:57 -0700

    Doing step 1 of UPER

--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -4,12 +4,59 @@ from hashtables import (HashTable,
                         hash_table_remove,
                         hash_table_retrieve,
                         hash_table_resize)
+"""
+UNDERSTANDING:
 
+    Reconstruct you trip from the jumbled up mass of flight tickets.
+    Each Ticket has a source and destination which are STRINGS
+
+    source = starting airport
+    destination = next airport
+
+    First Flight Ticket: {source: "NONE", destination: "airport"}
+    Last Flight Ticket: {source: "airport", destination: "NONE"}
+
+    None's a string? Yes
+
+    EXAMPLE:
+
+    tickets = [
+        Ticket{ source: "NONE", destination: "PDX" },
+        Ticket{ source: "PDX", destination: "DCA" },
+        Ticket{ source: "DCA", destination: "NONE" },
+        ]
+
+    Result = ["PDX", "DCA", "NONE"]
+
+    The Link is between the DESTINATION of PREVIOUS ticket and the SOURCE of NEXT ticket
+
+input = [tickets] and length
+output = an array of strings with the route of trip in order
+
+Solution should run in linear time
+assume function will always be handed a valid ticket chain as input
+
+HINTS:
+    NOTE: The crux of this problem requires us to 'link' tickets together to reconstruct the entire trip. For example, if we have a ticket ('SJC', 'BOS') that has us flying from San Jose to Boston, then there exists another ticket where Boston is the starting location, ('BOS', 'JFK').
+
+                   LINK               LINK               LINK
+                     |                  |                  |
+               _____________      _____________      _____________
+               |           |      |           |      |           |
+        s      d           s      d           s      d           s      d
+    ('NONE', 'SJC') --> ('SJC', 'BOS') --> ('BOS', 'JFK') --> ('JFK', 'NONE')
+
+    
+    NOTE: We can hash each ticket such that the starting location is the key and the destination is the value. Then, when constructing the entire route, the ith location in the route can be found by checking the hash table for the i-1th location.
+
+      key   :    value
+    [source : destination]
+"""
 
 class Ticket:
     def __init__(self, source, destination):
-        self.source = source
-        self.destination = destination
+        self.source = source                #starting airport
+        self.destination = destination      # next airport
 
 
 def reconstruct_trip(tickets, length):
@@ -21,3 +68,14 @@ def reconstruct_trip(tickets, length):
     """
 
     return route
+
+
+ticket_1 = Ticket("NONE", "PDX")
+ticket_2 = Ticket("PDX", "DCA")
+ticket_3 = Ticket("DCA", "NONE")
+
+tickets = [ticket_1, ticket_2, ticket_3]
+
+
+
+print(reconstruct_trip(tickets, 3))
--- hashtables/ex2/hashtables.py
+++ hashtables/ex2/hashtables.py
@@ -9,6 +9,9 @@ class LinkedPair:
         self.value = value
         self.next = None
 
+    def __repr__(self):
+        return f"Key = {self.key}, Value = {self.value}"
+
 
 # '''
 # Resizing hash table
@@ -17,6 +20,9 @@ class HashTable:
     def __init__(self, capacity):
         self.capacity = capacity
         self.storage = [None] * capacity
+
+    def __repr__(self):
+        return f"{self.storage}"
 
 
 # '''
